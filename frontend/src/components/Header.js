6c83e47027bfb37f354f1892da1632154787826f
shivam <shivam.infracoder@gmail.com>
2024-01-01 10:47:11 +0530

    version1 update with rating

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,15 +6,24 @@ function Header() {
   return (
     
         <header>
-            <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark" bg='' collapseOnSelect>
+            <Navbar expand="lg" className="bg-body-tertiary" bg='primary'  data-bs-theme="dark" collapseOnSelect>
                          <Container>
-                            <Navbar.Brand href="/">Keysaria Labels</Navbar.Brand>
+                            <Navbar.Brand href="/">
+                              Keysaria Labels
+                            {/* <img
+                                src="logo.jpeg"
+                                width="90"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
+                              /> */}
+                            </Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ms-auto">
                                 <Nav.Link href="#home"><i></i>Stiched</Nav.Link>
                                 <Nav.Link href="#home">Unstiched</Nav.Link>
-                               
+                                <></>
                                 <Nav.Link href="#home"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
                                 <Nav.Link href="#link"><i className='fas fa-user'></i>Login</Nav.Link>
                                
@@ -22,6 +31,9 @@ function Header() {
                             </Navbar.Collapse>
                           </Container>
             </Navbar>
+
+          
+
         </header>
     
   )
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import Rating from './Rating'
 
 function Product({product}) {
   return (
-    <Card className='my-3 p-3 rounded ' style={{ width: '20rem', transition: 'transform 0.3s ease-in-out',  }} border="warning">
+    <Card className='my-3 p-3 rounded ' style={{ width: '20rem' }} border="warning">
         <a href ={`/product/${product._id}`}>
             <Card.Img src = {product.image} variant='top' style={{height:'12rem'}}></Card.Img>
             {/* <Card.Title as="div"><strong>{product.name}</strong></Card.Title> */}
@@ -13,8 +14,8 @@ function Product({product}) {
                 
                 <div className='my-3'>
                 <Card.Title as="div"><strong>{product.name}</strong></Card.Title>
-                    {product.rating} from {product.numReviews} reviews
-
+                    
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}  color={'#f8e825'} />
                 </div>
             </Card.Text>
             <Card.Text as="h3">
